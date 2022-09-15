1f5a7ee0c7bd2465569bcb475f92a4bdb5cb75b7
Cecilia de la Parra <delaparracecilia@gmail.com>
2022-09-15 21:21:57 +0200

    iteracion 4

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
+    <button @click="addRandomContact">Add Random Contact</button>
+    <button @click ="sortByName">Sort by name</button>
+    <button @click ="sortByPopularity">Sort by popularity</button>
     <table>
       <tr>
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
       </tr>
       <tr v-for="data in topContacts" :key="data.name">
         <td>
-          <img
-            :src="data.pictureUrl"
-            :alt="`foto de ${data.name}`"
-          />
+          <img :src="data.pictureUrl" :alt="`foto de ${data.name}`" />
         </td>
         <td>{{ data.name }}</td>
         <td>{{ data.popularity }}</td>
+        <td v-if="data.wonOscar === true">&#127942</td>
+        <td v-else-if="data.wonOscar === false">Oh no &#128533</td>
+        <td v-if="data.wonEmmy === true">&#127942</td>
+        <td v-else-if="data.wonEmmy === false">Oh no &#128533</td>
       </tr>
     </table>
   </div>
@@ -26,13 +32,23 @@ export default {
   data() {
     return {
       contacts: json,
+      topContacts : json.slice(20, 25)
     };
   },
-  computed: {
-    topContacts() {
-      return [...this.contacts].slice(0, 5);
+  methods: {
+    addRandomContact() {
+        const filteredContacts = this.contacts.filter(contact => !this.topContacts.includes(contact))
+        const randomContact = filteredContacts[Math.floor(filteredContacts.length * Math.random())]
+        this.topContacts.push(randomContact)
+        console.log(randomContact)
     },
+    sortByName(){
+    this.topContacts.sort((a, b) => a.name.localeCompare (b.name) );
   },
+  sortByPopularity() {
+    this.topContacts.sort((a, b)=> b.popularity - a.popularity); 
+  }
+},
 };
 </script>
 
